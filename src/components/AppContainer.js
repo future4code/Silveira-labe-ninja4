903045b87c265ae48031ba2b3eda7ea5264eaa90
lucasquevedo1d <lucasquevedo1d@gmail.com>
2022-04-11 15:50:25 -0300

    teste

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
-
+import styled from 'styled-components'
+const Home = styled.div`
+background-color: red;
+`
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-        <p>Pronto para começar!</p>
+         
+        <h1>oi</h1>
+       
       </div>
     )
   }
